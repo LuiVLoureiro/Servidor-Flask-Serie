d06888dc80d33dc8f5f8c83f5ed2589030824f5a
LuiLoureiro <luivloureiro@gmail.com>
2024-05-12 21:39:58 -0300

    issue: Adicionar separador / de rota

--- main.py
+++ main.py
@@ -5,44 +5,48 @@ import os
 app = Flask(__name__)
 load_dotenv()
 
+print(os.getenv('EP1')) 
+
 @app.route('/')
 def index():
     return send_file('index.html')
 
-@app.route('ep1')
+@app.route('/ep1')
 def ep1():
     return send_file(os.getenv('EP1'))
-@app.route('ep2')
+
+@app.route('/ep2')
 def ep2():
     return send_file(os.getenv('EP2'))
-@app.route('ep3')
+
+@app.route('/ep3')
 def ep3():
     return send_file(os.getenv('EP3'))
-@app.route('ep4')
+@app.route('/ep4')
 def ep4():
     return send_file(os.getenv('EP4'))
-@app.route('ep5')
+@app.route('/ep5')
 def ep5():
     return send_file(os.getenv('EP5'))
-@app.route('ep6')
+@app.route('/ep6')
 def ep6():
     return send_file(os.getenv('EP6'))
-@app.route('ep7')
+@app.route('/ep7')
 def ep7():
     return send_file(os.getenv('EP7'))
-@app.route('ep8')
+@app.route('/ep8')
 def ep8():
     return send_file(os.getenv('EP8'))
-@app.route('ep9')
+@app.route('/ep9')
 def ep9():
     return send_file(os.getenv('EP9'))
-@app.route('ep10')
+@app.route('/ep10')
 def ep10():
     return send_file(os.getenv('EP10'))
-@app.route('ep11')
+@app.route('/ep11')
 def ep11():
     return send_file(os.getenv('EP11'))
-@app.route('ep12')
+@app.route('/ep12')
 def ep12():
     return send_file(os.getenv('EP12'))
 
